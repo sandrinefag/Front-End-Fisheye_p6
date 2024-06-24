eac095f84aa5ea24df8e5f1cd767177d64c86a70
sandrine fagour <sandrine94320@hotmail.fr>
2024-06-24 13:30:54 +0200

    récuperations des données ( cartes id photograpes) json

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,6 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait} = data;
+    console.log(name)
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +10,21 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const photographerTown = document.createElement('p');
+        photographerTown.classList.add('PhotographerPicture');
+        photographerTown.textContent = `${city}, ${country}`;
+        const photographerTagLine = document.createElement('p');
+        photographerTagLine.classList.add('PhotographerSlogan');
+        photographerTagLine.textContent = tagline;
+        const photographerPrice = document.createElement('p');
+        photographerPrice.classList.add('PhotographerPrice');
+        photographerPrice.textContent = `${price}€ /jours`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(photographerTown);
+        article.appendChild(photographerTagLine);
+        article.appendChild(photographerPrice);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
