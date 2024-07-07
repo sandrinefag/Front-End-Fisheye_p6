6f2ed245e5a55fd189527d75a4fb9fbe9b1f85d4
sandrine fagour <sandrine94320@hotmail.fr>
2024-07-07 17:18:04 +0200

    Creation lien entre index et pages photographe. Intégration de la page photographes(info et gallerie photos)

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,109 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// //Mettre le code JavaScript lié à la page photographer.html
+async function loadPhotographerData() {
+    try {
+        
+        const photographerId = getPhotographerIdFromURL();
+      
+
+        const {photographer, mediasPhoto } = await fetchPhotographerById(photographerId);
+
+        // Afficher les informations du photographe
+        displayPhotographerInfos(photographer, mediasPhoto);
+    } catch (error) {
+        console.error('Erreur lors du chargement des données du photographe:', error);
+    }
+}
+
+// Appeler la fonction pour charger les données du photographe
+loadPhotographerData();
+
+
+function getPhotographerIdFromURL() {
+    return new URL(location.href).searchParams.get("id");
+ 
+}
+
+
+
+// Fonction asynchrone pour récupérer les données du photographe depuis le fichier JSON
+async function fetchPhotographerById(photographerId) {
+    try {
+        const response = await fetch("../../data/photographers.json");
+        const data = await response.json();
+        // Trouver le photographe correspondant à l'ID
+        const photographer = data.photographers.find(photograph => photograph.id == photographerId);
+        const mediasPhoto = data.media.filter(works => works.photographerId == photographerId)
+        return {photographer, mediasPhoto};
+      
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données du photographe:', error);
+    }
+  
+}
+  
+
+// Fonction pour afficher les informations du photographe
+function displayPhotographerInfos(photographer, mediasPhoto) {
+    console.log(mediasPhoto)
+    console.log(photographer)
+    if (!photographer) {
+        console.error('Aucun photographe trouvé avec cet ID.');
+        return;
+    }
+    const photographerInfosPart = document.createElement('div')
+    photographerInfosPart.classList.add('photographerInfos')
+    const photographerPhotoContact = document.createElement('div')
+    photographerPhotoContact.classList.add('photograph-photoContact')
+    const photographContact = document.querySelector('.contact_button')
+    const photographerPicture = `assets/photographers/${photographer.portrait}`
+    const photographerHeader = document.querySelector(`.photograph-header`);
+    const photographerName = document.createElement('h1')  
+    photographerName.classList.add(`photographer-photo`)
+    photographerName.innerText = photographer.name
+    const photographerCity = document.createElement('p')
+    photographerCity.innerText = `${photographer.city}, ${photographer.country}`
+    const photographerTagLine = document.createElement('p')
+    photographerTagLine.innerText = photographer.tagline
+    const photographerPhoto = document.createElement('img')
+    photographerPhoto.classList.add('photographerPhoto')
+    photographerPhoto.setAttribute("src", photographerPicture)
+
+   
+    photographerHeader.appendChild(photographerPhotoContact)
+    photographerPhotoContact.appendChild(photographerPhoto)
+    photographerPhotoContact.appendChild(photographContact)
+    photographerHeader.appendChild(photographerInfosPart)
+    photographerInfosPart.appendChild(photographerName)
+    photographerInfosPart.appendChild(photographerCity)
+    photographerInfosPart.appendChild(photographerTagLine)
+    
+    //photographer's works
+    if (mediasPhoto.length > 0){
+     mediasPhoto.forEach(media => {
+         const photographerWorks = `assets/images/${encodeURIComponent(photographer.name)}-photos/${encodeURIComponent(media.image)}`
+         const photosGallery = document.querySelector(`.photographerWorks`)
+         const photosDiv = document.createElement(`div`)
+        const gallery = document.createElement(`img`)
+        gallery.classList.add('photographers-works')
+        gallery.setAttribute("src", photographerWorks)
+         const photoName = document.createElement('p')
+         photoName.innerText = `${media.title}`
+         const photoLike = document.createElement(`p`)
+         photoLike.innerText = `${media.likes}`
+         
+         
+
+         photosGallery.appendChild(photosDiv)
+         photosDiv.appendChild(gallery)
+         photosDiv.appendChild(photoName)
+         photosDiv.appendChild(photoLike)
+      
+         
+    })
+   
+    } else {
+        console.log('no media found for this photographer')
+    }
+
+}
 
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,27 +5,45 @@ function photographerTemplate(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        //creation d'une balise a href pour rendre l'article cliquable
+        const articleLink = document.createElement('a');
+        articleLink.setAttribute('href', `../photographer.html?id=${id}`);
+        const article = document.createElement('article');
+        
+
+        //division en 2 de l'article. Une div qui contient la photo et nom du photographe. Et une autre qui contient le reste des infos de celui-ci
+        //1ere partie
+        const idPhotographerPart = document.createElement('div');
+        idPhotographerPart.classList.add('photoNamePhotographer');
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        //2eme partie
+        const photographerInfo = document.createElement('div');
+        photographerInfo.classList.add('photographerInfo');
         const photographerTown = document.createElement('p');
-        photographerTown.classList.add('PhotographerPicture');
+        photographerTown.classList.add('photographerTown');
         photographerTown.textContent = `${city}, ${country}`;
         const photographerTagLine = document.createElement('p');
-        photographerTagLine.classList.add('PhotographerSlogan');
+        photographerTagLine.classList.add('photographerSlogan');
         photographerTagLine.textContent = tagline;
         const photographerPrice = document.createElement('p');
-        photographerPrice.classList.add('PhotographerPrice');
+        photographerPrice.classList.add('photographerPrice');
         photographerPrice.textContent = `${price}€ /jours`;
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(photographerTown);
-        article.appendChild(photographerTagLine);
-        article.appendChild(photographerPrice);
-
-        return (article);
+        
+        
+        articleLink.appendChild(article);
+        article.appendChild(idPhotographerPart);
+        idPhotographerPart.appendChild(img);
+        article.appendChild(photographerInfo);
+        
+        idPhotographerPart.appendChild(h2);
+        photographerInfo.appendChild(photographerTown);
+        photographerInfo.appendChild(photographerTagLine);
+        photographerInfo.appendChild(photographerPrice);
+        return (articleLink);
     }
     return { name, picture, getUserCardDOM }
 }
