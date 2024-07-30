ae5950051f4a8553f73162be66031ae7f257613b
sandrine fagour <sandrine94320@hotmail.fr>
2024-07-30 22:09:04 +0200

    fix url photographer.js

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,7 +7,7 @@ function photographerTemplate(data) {
 	function getUserCardDOM() {
 		//creation d'une balise a href pour rendre l'article cliquable
 		const photographerPageLink = document.createElement("a");
-		photographerPageLink.setAttribute(`href`, `../photographer.html?id=${id}`);
+		photographerPageLink.setAttribute(`href`, `photographer.html?id=${id}`);
 		const article = document.createElement("article");
 
 		//division en 2 de l'article. Une div qui contient la photo et nom du photographe. Et une autre qui contient le reste des infos de celui-ci
